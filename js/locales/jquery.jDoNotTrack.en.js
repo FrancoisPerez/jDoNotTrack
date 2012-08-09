a0e7009c35e2392fcb01dc9665023142f1ba7554
François Pérez <banke77@gmail.com>
2012-08-09 15:57:35 +0200

    Fixed link

--- js/locales/jquery.jDoNotTrack.en.js
+++ js/locales/jquery.jDoNotTrack.en.js
@@ -1,6 +1,6 @@
 $.doNotTrack.options.lang = {
-	'1': 'Hey ! Seems like you are using the <strong>"Do Not Track"</strong> setting of your browser.<br />Did you know that, because of that, you are probably missing some very interesting informations and offers ?<br /><a href="https://raw.github.com/FrancoisPerez/jDoNotTrack/master/doNotTrack.html" target="_blank" style="color:#FFF;text-decoration:underline">Click here</a> to find out how to disable it !',
-	'2': 'Hello ! That\'s really a shame that you use the <strong>"Do Not Track"</strong> setting of your browser.<br />Did you know that, because of that, you are probably missing some very interesting informations and offers ?<br /><a href="https://raw.github.com/FrancoisPerez/jDoNotTrack/master/img/jDoNotTrack.png" target="_blank" style="color:#FFF;text-decoration:underline">Click here</a> to find out how to disable it !',
+	'1': 'Hey ! Seems like you are using the <strong>"Do Not Track"</strong> setting of your browser.<br />Did you know that, because of that, you are probably missing some very interesting informations and offers ?<br /><a href="http://francoisperez.github.com/jDoNotTrack/doNotTrack.html" target="_blank" style="color:#FFF;text-decoration:underline">Click here</a> to find out how to disable it !',
+	'2': 'Hello ! That\'s really a shame that you use the <strong>"Do Not Track"</strong> setting of your browser.<br />Did you know that, because of that, you are probably missing some very interesting informations and offers ?<br /><a href="http://francoisperez.github.com/jDoNotTrack/doNotTrack.html" target="_blank" style="color:#FFF;text-decoration:underline">Click here</a> to find out how to disable it !',
 };
 $.doNotTrack.options.layout = {
 	'closeAlert':'Close this alert'
