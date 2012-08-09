9f5b9f3b323f79d15d6c27425e19bad67b1f82d1
François Pérez <banke77@gmail.com>
2012-08-09 14:16:02 +0200

    Version 1.0.1

--- js/jquery.jDoNotTrack.js
+++ js/jquery.jDoNotTrack.js
@@ -1,15 +1,15 @@
 /*!
  * jQuery Do Not Track (DNT) plugin
  * Educational jQuery Plugin for Do Not Track setting
- * Copyright (c) 2012 F. Pérez
- * Version: 1.0.0 (8-AUG-2012)
+ * Copyright (c) 2012 F. Pérez (@FrancoisPerez)
+ * Version: 1.0.1 (8-AUG-2012)
  * Dual licensed under the MIT and GPL licenses.
  * Requires: jQuery v1.3.2 or later
  */
 ;(function($, undefined){
 	'use strict';
 	
-	var ver = '1.0.0';
+	var ver = '1.0.1';
 	var dntEnabled = false;
 	
 	function log() {
@@ -31,35 +31,40 @@
 		}
 	}
 			
-	
+	// Default options
 	$.doNotTrack = {
 		options: {
-			zindex: '99999',
-			height: '70',
-			id: 'doNotTrackAlert',
-			idCloseButton: 'doNotTrackCloseButton',
-			autoHide: false,
+			zindex:'99999',
+			height:'70',
+			id:'doNotTrackAlert',
+			idCloseButton:'doNotTrackCloseButton',
+			autoHide:true,
 			timeBeforeShow: 0,
 			animSpeed:1000,
 			timeBeforeHide:10000,
+			opacity:0.75,
+			backgroundColor:"#000",
 		},
 		elements: []
 	};
-
-	/*if($.browser.msie && $.browser.version < 7){
-		$.support.selectOpacity = false;
-	}else{
-		$.support.selectOpacity = true;
-	}*/
 	
 	$.fn.doNotTrack = function(options) {
 		options = $.extend($.doNotTrack.options, options);
 		
+  
 		detectDNT();
 		
-		if(dntEnabled){
+		//if(dntEnabled){
+			
+			// Checks if opacity css is supported
+			if($.browser.msie && $.browser.version < 7){
+				$.support.selectOpacity = false;
+			}else{
+				$.support.selectOpacity = true;
+			}
+			
 			createAlert();
-		}
+		//}
 	}
 	
 	function createAlert() {
@@ -69,16 +74,17 @@
 
 		var close = $('<a/>',{
 			href : '#',
-			alt : $.doNotTrack.options.lang['closeAlert'],
-			title: $.doNotTrack.options.lang['closeAlert'],
+			alt : $.doNotTrack.options.layout['closeAlert'],
+			title: $.doNotTrack.options.layout['closeAlert'],
 			id: $.doNotTrack.options.idCloseButton,
 			css : {
 				'position':'absolute',
-				'left':'95%',
-				'top': ($.doNotTrack.options.height/2) -15,
+				'right':'1%',
+				'top': ($.doNotTrack.options.height/2) -15 + 'px',
 				'cursor':'pointer',
 				'color':'#FFF',
 				'text-decoration':'none',
+				'font': 'normal bold 14px Arial,Helvetica,sans-serif',
 			},
 			click: function(){
 				$('#' + $.doNotTrack.options.id).animate({
@@ -88,14 +94,14 @@
 			},
 			text:'[x]'
 		})
+
 		var div = $('<p/>', {
 			css: {
 				'height': $.doNotTrack.options.height + 'px',
 				'width': '1%',
 				'color': '#FFF',
 				'text-align':'center',
-				'font-family':'Arial, Helvetica, sans-serif',
-				'font-size' : '12px',
+				'font': ' normal 12px Arial,Helvetica,sans-serif',
 				'display': 'table-cell',
 				'vertical-align': 'middle',
 			},
@@ -111,9 +117,13 @@
 				'left':0,
 				'width': '100%',
 				'height': $.doNotTrack.options.height + 'px',
-				'background-color':	'#000',
+				'background-color':	$.doNotTrack.options.backgroundColor,
 				'display':'none',
-				'z-index': $.doNotTrack.options.zindex
+				'z-index': $.doNotTrack.options.zindex,
+				'opacity': function(){
+					if($.support.selectOpacity)	return $.doNotTrack.options.opacity;
+					else return '1';
+				}
 			},
 			html: div
 		})
